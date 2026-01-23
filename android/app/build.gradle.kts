0495eaf2922e0914518f1e1d79dd136aaefc8ea4
Hardik Jain <jainhardik120@gmail.com>
2026-01-23 18:02:57 +0530

    fix: update minSdk version to 26 in build configuration

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.jainhardik120.expensetracker"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
